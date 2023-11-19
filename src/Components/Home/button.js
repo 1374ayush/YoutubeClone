0ea27f70c60d6ee8d95674333c3e8dd2362e0922
Ayush <7456ayush@gmail.com>
2023-11-19 21:02:31 +0530

    buttons created

--- src/Components/Home/button.js
+++ src/Components/Home/button.js
@@ -4,8 +4,20 @@ import './homeCss.css'
 const Button = () =>{
     return (
         <>
-            <div>
-                Button
+            <div className="buttonContainer">
+            <button type="button" className="button" style={{backgroundColor:'black',color:'white', marginLeft:15}}>All</button>
+            <button type="button" className="button">Live</button>
+            <button type="button" className="button">Sport</button>
+            <button type="button" className="button">Javascript</button>
+            <button type="button" className="button">WebDevelopment</button>
+            <button type="button" className="button">Code</button>
+            <button type="button" className="button">Music</button>
+            <button type="button" className="button">BrosCode</button>
+            <button type="button" className="button">AI</button>
+            <button type="button" className="button">OpenAI</button>
+            <button type="button" className="button">Final</button>
+            <button type="button" className="button">Code</button>
+            <button type="button" className="button">Code</button>
             </div>
         </>
     )
