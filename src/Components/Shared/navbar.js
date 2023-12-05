aa41b616d6eadebccbf544ffd0d973844cf530c2
Ayush <7456ayush@gmail.com>
2023-12-05 17:55:46 +0530

    completed the search functionality with the ui

--- src/Components/Shared/navbar.js
+++ src/Components/Shared/navbar.js
@@ -1,38 +1,89 @@
-import HamBurIcon from '../../images/hamburIcon.png'
-import youtubeLogo from '../../images/youtube.png'
-import './navCss.css'
-import person from '../../images/person.png'
+import HamBurIcon from "../../images/hamburIcon.png";
+import youtubeLogo from "../../images/youtube.png";
+import "./navCss.css";
+import person from "../../images/person.png";
+import axios from "axios";
+import { youtube_search_api } from "../../constants/youttubeSearchApi.js";
 
-import { useDispatch } from 'react-redux'
-import { toggleMenu } from '../../utils/sidebarSlice'
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../../utils/sidebarSlice";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const [searchVal, setSearchVal] = useState("");
+  const [searchData, setSearchData] = useState([])
+  const [searchBlock, setSearchBlock] = useState(false);
+  console.log(searchVal);
 
-  const dispatch = useDispatch()
+  const toggleMenuHandler = () => {
+    console.log("hello");
+    dispatch(toggleMenu());
+  };
 
-  const toggleMenuHandler = () =>{
-    console.log("hello")
-      dispatch(toggleMenu())
-  }
+  useEffect(() => {
+    const timer = setTimeout(() => searchSuggestions(), 200);
+
+    return () => clearTimeout(timer);
+  }, [searchVal]);
+
+  const searchSuggestions = () => {
+    axios
+      .get(youtube_search_api + searchVal)
+      .then((response) => {
+        setSearchData(response.data[1]);
+        console.log(searchData)
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <>
-      <div className='navSet mx-3'>
-        <ul className='navUl'> 
-            <img src={HamBurIcon} className='navImg' onClick={()=>toggleMenuHandler()}/> 
-             <a href='#'><img src={youtubeLogo} className='navLogo' /></a>
-            <li className='navList ml-4 rounded-lg'> <input type="text" className="navSearch" placeholder='Search'/></li>
-            
-            {/* <li className='navList ml-4 rounded-lg'> <img src={search} className='navImg'/> </li> */}
-          </ul>
+      <div className="navSet mx-3">
+        <ul className="navUl">
+          <img
+            src={HamBurIcon}
+            className="navImg"
+            onClick={() => toggleMenuHandler()}
+          />
+          <a href="#">
+            <img src={youtubeLogo} className="navLogo" />
+          </a>
+          <li className="navList ml-4 rounded-lg">
+            <input
+              type="text"
+              className="navSearch"
+              placeholder="Search"
+              onChange={(e) => setSearchVal(e.target.value)}
+              onFocus={() => setSearchBlock(true)}
+              onBlur={() => {setSearchBlock(false); setSearchVal("")}}
+            />
 
-          <ul className='navUl'> 
-          <li className='navList'> <img src={person} className='navImg'/></li>
-          </ul>
+            {searchBlock && <div className="searchSuggestion py-2">
+              <ul className="searchSuggestionUl">
+                {searchData.map((item, index) => (
+                  <li className="searchSuggestionLi px-2" key={index}>
+                    <div className="searchContent">
+                      {item}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>}
+          </li>
 
+          {/* <li className='navList ml-4 rounded-lg'> <img src={search} className='navImg'/> </li> */}
+        </ul>
+
+        <ul className="navUl">
+          <li className="navList">
+            {" "}
+            <img src={person} className="navImg" />
+          </li>
+        </ul>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- src/constants/youttubeSearchApi.js
+++ src/constants/youttubeSearchApi.js
@@ -1,3 +1,3 @@
-const youtube_search_api="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const youtube_search_api="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 //you need to put the word for search suggestion.
 //this is basically a search suggestion Api
