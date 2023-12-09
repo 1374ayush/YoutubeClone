3b9bb3cd47c7d35b3568d0b070efc4485cdcf0fc
Ayush <7456ayush@gmail.com>
2023-12-09 23:29:50 +0530

    created slice for caching search operation

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import sideBarSlice from './sidebarSlice.js';
+import searchSlice from "./searchSlice.js";
 
 const store = configureStore({
     reducer:{
         sidebar:sideBarSlice,
+        search: searchSlice,
     }
 })
 
